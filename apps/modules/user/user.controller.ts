ba325c09f0dfd04173e99ac90ee0e5001a9f7c2c
linhnv2 <linhnv2@rikkeisoft.com>
2023-05-11 17:37:33 +0700

    [DECORATOR]

--- apps/modules/user/user.controller.ts
+++ apps/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Put, Req } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Put, Req } from '@nestjs/common';
 import { Exception } from '@root/libs/core/exception/Exception';
 import { RequestT } from '@root/libs/core/request';
 import { UserService } from './user.service';
@@ -9,6 +9,8 @@ import {
 } from '@root/apps/dto/response';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ApiExceptionResponse, ApiPaginatedResponse } from '@root/apps/decorator/response.decorator';
+import { AuthDecorator } from '@root/apps/decorator/auth.decorator';
+import { Auth } from '../auth/common';
 @ApiTags('users')
 @Controller('users')
 export class UserController {
@@ -18,9 +20,12 @@ export class UserController {
   @ApiBearerAuth()
   @ApiPaginatedResponse(AccountDTO)
   @ApiExceptionResponse()
-  async index(@Req() req: RequestT): Promise<PaginatedResponse<AccountDTO>> {
+  async index(
+    @Req() req: RequestT,
+    @AuthDecorator() auth: Auth,
+  ): Promise<PaginatedResponse<AccountDTO>> {
     try {
-      console.log('Request.users.index', req.auth, req.auth.permissions);
+      console.log('Request.users.index', auth, auth.permissions);
       const result = await this.userService.index();
       return result;
     } catch (e) {
@@ -32,9 +37,14 @@ export class UserController {
   @Put('/:id')
   @ApiBearerAuth()
   @ApiExceptionResponse()
-  async edit(@Req() req: RequestT): Promise<UserUpdateResponse> {
+  async edit(
+    @Req() req: RequestT,
+    @Param('id', ParseIntPipe) id: number,
+    @AuthDecorator() auth: Auth,
+  ): Promise<UserUpdateResponse> {
     try {
-      console.log('Request.users.edit', req.auth, req.auth.permissions);
+      console.log("id", id);
+      console.log('Request.users.edit', auth, auth.permissions);
 
       const result = await this.userService.edit();
       return result;
